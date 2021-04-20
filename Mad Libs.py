b3ef436199fc1c4baf09e4cc0808c95263f650f2
nikilpawar27 <82569668+nikilpawar27@users.noreply.github.com>
2021-04-20 20:25:18 +0530

    Add files via upload

---
+++ Mad Libs.py
@@ -0,0 +1,14 @@
+adjective= input("Adjective:")
+adjective2= input("Adjective:")
+adjective3= input("Adjective:")
+adjective4= input("Adjective:")
+adjective5= input("Adjective:")
+adverb= input("Verb:")
+adverb2= input("Verb:")
+adverb3= input("Verb:")
+adverb4= input("Verb:")
+adverb5= input("Verb:")
+adverb6= input("Verb:")
+
+Story=f"It was Christmas Eve and  {adjective} girls and boys everywhere were hungrily {adverb} awaiting Santa's arrival.\n The elves worked  {adverb2} to put the finishing touches on all the toys. The  {adjective2} reindeer ran  {adverb3} into position at the head of Santa's sleigh. They soared into the midnight sky, high above the  {adjective3} rooftops. \nSanta laughed  {adverb4} as he delivered  {adjective4} presents to all the children's homes. {adverb5} wrapped gifts glistened under the  {adjective5} lights of Christmas trees.\n Children around the world awake early on Christmas morning and  {adverb6} tiptoed out of bed to see if Santa had come."
+print(Story)
